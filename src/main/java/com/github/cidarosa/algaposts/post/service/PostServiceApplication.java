c0be2bbd08f97a79a00de7a3b8616faacb3f5184
Cida Rosa <lucita.rosa@gmail.com>
2026-07-07 22:05:44 -0300

    feat: new project files - init commit spring

---
+++ src/main/java/com/github/cidarosa/algaposts/post/service/PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.cidarosa.algaposts.post.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cidarosa/algaposts/post/service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cidarosa.algaposts.post.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
